05763f4509b6ab400bc79b4164420100aea2e720
Charlotte Fereday <cbrfereday@gmail.com>
2017-07-10 23:22:12 +0100

    Adds withdrawal_statement_collector hash to store details of the withdrawal

--- lib/bank_account_statement.rb
+++ lib/bank_account_statement.rb
@@ -1,7 +1,7 @@
 class BankAccountStatement
-  attr_reader :deposit_statement_collector
+  attr_reader :deposit_statement_collector, :withdrawal_statement_collector
 
   def initialize
     @deposit_statement_collector = { date: '', credit: 0, balance: 0 }
+    @withdrawal_statement_collector = { date: '', debit: 0, balance: 0}
   end
-end
--- spec/bank_account_statement_spec.rb
+++ spec/bank_account_statement_spec.rb
@@ -6,6 +6,12 @@ describe BankAccountStatement do
   describe '#initialize' do
     it 'is initialized with a deposit statement collector' do
       expect(bank_account_statement.deposit_statement_collector).to equal bank_account_statement.deposit_statement_collector
+      # expect(bank_account_statement.deposit_statement_collector).to equal { date: '', credit: 0, balance: 0 }
+    end
+
+    it 'is initialized with a withdrawal statement collector' do
+      expect(bank_account_statement.withdrawal_statement_collector).to equal bank_account_statement.withdrawal_statement_collector
+      # expect(bank_account_statement.withdrawal_statement_collector).equal? { date: '', debit: 0, balance: 0}
     end
   end
 end
